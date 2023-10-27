4159ede070014eb0fae2b18440e28ebce0d43bd3
Adeyemi Dada <yemidada@gmail.com>
2023-10-27 10:28:50 +0100

    Update User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :entities, class_name: 'entity', foreign_key: 'author_id', dependent: :destroy
+  has_many :groups, dependent: :destroy
   validates :name, presence: true
 end
